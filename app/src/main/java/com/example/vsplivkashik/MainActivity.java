4d2df01a8a8cbe5a8e8ffe77270d1db5f9f55280
igorgolenisev1317@gmail.com <igorgolenisev1317@gmail.com>
2024-03-27 15:38:43 +0300

    Add project

---
+++ app/src/main/java/com/example/vsplivkashik/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.vsplivkashik;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToNextPage1(View view) {
+        Intent i = new Intent(this, MainActivity2.class);
+        startActivity(i);
+    }
+
+    public void goToNextPage2(View view) {
+        Intent i2 = new Intent(this, MainActivity3.class);
+        startActivity(i2);
+    }
+
+}
---
+++ app/src/main/java/com/example/vsplivkashik/MainActivity3.java
@@ -0,0 +1,21 @@
+package com.example.vsplivkashik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    private EditText editTextEmailAddress;
+    private EditText editTextEmailText;
+    private Button buttonSend;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+    }
+}
