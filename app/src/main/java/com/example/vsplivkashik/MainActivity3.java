c9c8ab7a3d7009cf06f8bb6a68c59cf19642e725
igorgolenisev1317@gmail.com <igorgolenisev1317@gmail.com>
2024-04-08 20:09:02 +0300

    Email dispatch

--- app/src/main/java/com/example/vsplivkashik/MainActivity3.java
+++ app/src/main/java/com/example/vsplivkashik/MainActivity3.java
@@ -1,23 +1,42 @@
 package com.example.vsplivkashik;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity3 extends AppCompatActivity {
 
-    private EditText editTextEmailAddress;
-    private EditText editTextEmailText;
-    private Button buttonSend;
+    Button button;
+    EditText sendto, subject, body;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
-    }
+        sendto = findViewById(R.id.editTextTextEmailAddress);
+        subject = findViewById(R.id.editTextText);
+        body = findViewById(R.id.editTextEmailText);
+        button = findViewById(R.id.buttonSend);
 
-    
+        button.setOnClickListener(view -> {
+            String emailsend = sendto.getText().toString();
+            String emailsubject = subject.getText().toString();
+            String emailbody = body.getText().toString();
+
+            Intent intent = new Intent(Intent.ACTION_SEND);
+
+            intent.putExtra(Intent.EXTRA_EMAIL, new String[]{emailsend});
+            intent.putExtra(Intent.EXTRA_SUBJECT, emailsubject);
+            intent.putExtra(Intent.EXTRA_TEXT, emailbody);
+
+            intent.setType("message/rfc822");
+
+            intent.setPackage("com.google.android.gm");
+
+            startActivity(intent);
+        });
+    }
 }
